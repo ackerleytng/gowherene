0d692b615188ba8097504025b4b0f071e0960df9
Ackerley Tng <ackerleytng@gmail.com>
2017-12-23 00:26:47 +0800

    Add map and rendering to client side

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,12 +1,39 @@
+const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var map;
+
+function initMap() {
+  const singapore = {lat: 1.352083, lng: 103.819836};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: singapore
+  });
+}
+
 function plotHandler(e) {
   e.preventDefault();
  
-  var url = $('input#url').val();
+  const url = $('input#url').val();
   
   console.log(["request", url]);
   
-  $.get("/parse", {"url": url}, function(data) {
+  $.getJSON("/parse", {"url": url}, function(data) {
     console.log(["response", data]);
+
+    data.map(function(item, i) {
+      var infowindow = new google.maps.InfoWindow({
+        content: `<p><b>${item["place"]}</b></p>` +
+          `<p>${item["address"]}</p>`
+      });
+      var marker = new google.maps.Marker({
+        position: item["latlng"],
+        label: labels[i % labels.length],
+        map: map,
+        title: item["place"]
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    });
   });
   
   return false;
