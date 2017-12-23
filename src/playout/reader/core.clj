23a3aab7d4032a4dbcd694f30651aa76cb87fefa
Ackerley Tng <ackerleytng@gmail.com>
2017-12-23 08:58:38 +0800

    Expand regex for postal code
    Grabs addresses more accurately

--- src/playout/reader/core.clj
+++ src/playout/reader/core.clj
@@ -8,7 +8,7 @@
             [hickory.select :as s]
             [environ.core :refer [env]]))
 
-(def re-postal-code #"[sS]\(?\d{6}\)?")
+(def re-postal-code #"[sS](?:ingapore)?\s*\(?\d{6}\)?")
 
 (defn url->hickory
   [url]
@@ -30,35 +30,41 @@
 (defn- get-content
   "Given a node, return all content in a string"
   [node]
-  (if-let [content (:content node)]
-    (str/join (map get-content content))
+  (if (= :element (:type node)) 
+    (if-let [content (:content node)]
+      (str/join (map get-content content))
+      "")
     node))
+
+(defn- loc->address
+  "Given a loc, return the address"
+  [hloc-zip]
+  (->> hloc-zip
+       zip/down
+       (iterate zip/next)
+       ;;(take-while identity)
+       (map zip/node)
+       (map str)
+       (drop-while #(not (re-find re-postal-code %)))
+       first))
 
 (defn hickory->data
   "Takes a hickory and returns a data of all the places and addresses on the page"
   [hickory]
   (let [postal-code-locs (get-postal-code-locs hickory)
-        header-locs (map get-earlier-header postal-code-locs)
+        header-locs (filter identity (map get-earlier-header postal-code-locs))
         headers (->> header-locs
                      (map zip/node)
                      (map get-content)
                      (map #(str/replace % #"[\u00a0\s]+" " "))
                      (map str/trim))
         addresses (->> postal-code-locs
-                       (map zip/node)
-                       (map get-content)
+                       (map loc->address)
                        (map #(str/replace % #"[\u00a0\s]+" " "))
                        (map str/trim))]
     (map #(reduce (fn [r [k v]] (assoc r k v)) {} (partition 2 %)) 
          (partition 4 (interleave (repeat :place) headers
                                   (repeat :address) addresses)))))
-
-(defn data-add-geocode
-  "Takes a data and adds on geocoding"
-  [data]
-  (pmap (fn [{:keys [place address] :as d}] 
-          (assoc d :latlng (geocode address)))
-        data))
 
 (defn geocode
   [address]
@@ -68,3 +74,11 @@
       :body
       json/read-json
       (get-in [:results 0 :geometry :location])))
+
+(defn data-add-geocode
+  "Takes a data and adds on geocoding"
+  [data]
+  (pmap (fn [{:keys [place address] :as d}] 
+          (assoc d :latlng (geocode address)))
+        data))
+
