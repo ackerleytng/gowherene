34149ce3e296da62a0e7ac0765e952b7df38ad6b
Ackerley Tng <ackerleytng@gmail.com>
2018-03-31 12:35:02 +0800

    Add description to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject gowherene "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "gowherene plots addresses from recommendation pages in Singapore on a map!"
+  :url "https://gowherene.herokuapp.com"
   :min-lein-version "2.0.0"
   :jvm-opts ["--add-modules" "java.xml.bind"]
-
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
