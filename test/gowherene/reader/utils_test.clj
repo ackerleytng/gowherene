59230db084c6b0012db3527b583e0be6eab895b7
ackerleytng <ackerleytng@gmail.com>
2019-04-08 20:57:42 +0800

    Add test for content

--- test/gowherene/reader/utils_test.clj
+++ test/gowherene/reader/utils_test.clj
@@ -1,5 +1,8 @@
 (ns gowherene.reader.utils-test
   (:require [clojure.test :refer :all]
+            [hickory.zip :refer [hickory-zip]]
+            [hickory.convert :refer [hiccup-to-hickory]]
+            [clojure.zip :as zip]
             [gowherene.reader.utils :refer :all]))
 
 (deftest test-word-count
@@ -10,3 +13,17 @@
     (is (= 1 (word-count "testing")))
     (is (= 2 (word-count "testing     one")))
     (is (= 3 (word-count "one two three")))))
+
+(deftest test-content
+  (testing "content"
+    (is (= "header hello"
+           (->> (hickory-zip (hiccup-to-hickory [[:h1 "header"] [:p "hello"]]))
+                content))))
+  (testing "content strips extra spaces"
+    (is (= "header hello"
+           (->> (hickory-zip (hiccup-to-hickory [[:h1 "header"] "        " [:p "hello"]]))
+                content))))
+  (testing "content works recursively"
+    (is (= "header hello world"
+           (->> (hickory-zip (hiccup-to-hickory [[:h1 "header"] [:p [:b "hello"] [:i "world"]]]))
+                content)))))
