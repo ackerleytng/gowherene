cf136c63929aabdf125749d77d30f4268694630f
ackerleytng <ackerleytng@gmail.com>
2019-04-14 11:53:10 +0800

    Implement building-number detection and building up of address parts

--- src/gowherene/reader/location.clj
+++ src/gowherene/reader/location.clj
@@ -11,12 +11,28 @@
   [string]
   (re-find re-unit-number string))
 
+(defn building-number
+  [string road-name]
+  (when-let [matches (re-find (re-pattern (str "(\\d+)\\s*" road-name)) string)]
+    (second matches)))
+
 (defn address-parts
   [string]
-  nil)
+  (let [postal-code* (postal-code string)
+        unit-number* (unit-number string)
+        road-name* (road-name string)
+        parts {:postal-code postal-code*
+               :unit-number unit-number*
+               :road-name road-name*}]
+    (assoc
+     parts
+     :building-number
+     (when road-name*
+       (building-number string road-name*)))))
 
 (defn add-location
   [{:keys [loc type] :as input}]
   (case type
-    :postal-code (assoc input :location (zip/node loc))
+    :postal-code (assoc input :location (address-parts (zip/node loc)))
+    :labelled (assoc input :location (address-parts (:value input)))
     input))
--- test/gowherene/reader/location_test.clj
+++ test/gowherene/reader/location_test.clj
@@ -101,3 +101,101 @@
 
       "1 Maritime Square #04-01 HarbourFront Centre"
       "Maritime Square")))
+
+(deftest test-building-number
+  (testing "building-number"
+    (are [string road-name expected] (= expected (building-number string road-name))
+      "258 Tanjong Katong Road |"
+      "Tanjong Katong Road"
+      "258"
+
+      "#08-01/02/03, Orchard Central, 181 Orchard Road |"
+      "Orchard Road"
+      "181"
+
+      "101 Thomson Road, Singapore 307591 (United Square Outlet) | 6254 8123 | Opening Hours for Lunch: 11:30am ~ 2:30pm (Daily Except PH) for Dinner: Sun ~ Thur, PH : 5:30pm ~ 12am Fri, Sat, Day Before PH : 5:30pm ~ 2am"
+      "Thomson Road"
+      "101"
+
+      "7 Tan Quee Lan St, Singapore 188096 | Tel number: +65 63362875 | Open daily from 12pm – 12am"
+      "Tan Quee Lan St"
+      "7"
+
+      "181 Orchard Road, 08-09/10/11 Orchard Central, Singapore 238896 | Opening hours: Mon – Fri: 11:00 – 15:00 and 18:00 – 22:00 Sat – Sun: 11:30 – 22:30 |"
+      "Orchard Road"
+      "181"
+
+      "School Of The Arts Singapore, 1 Zubir Said Drive #01-04/07 Singapore 227968 (Dhoby Ghaut) | Tel: 6238 7218 | Opening hours: Mon – Fri: 11:30 – 14:30, Mon – Fri: 17:00 – 23:00 and Sat – Sun: 11:30 – 23:00"
+      "Zubir Said Drive"
+      "1"
+
+      "Plaza Singapura #03-01 68 Orchard Road Singapore 238839 |"
+      "Orchard Road"
+      "68"
+
+      "1 Maritime Square #04-01 HarbourFront Centre"
+      "Maritime Square"
+      "1")))
+
+(deftest test-address-parts
+  (testing "address-parts"
+    (are [string expected] (= expected (address-parts string))
+      "258 Tanjong Katong Road |"
+      {:postal-code nil
+       :unit-number nil
+       :road-name "Tanjong Katong Road"
+       :building-number "258"}
+
+      "#08-01/02/03, Orchard Central, 181 Orchard Road |"
+      {:postal-code nil
+       :unit-number "#08-01/02/03"
+       :road-name "Orchard Road"
+       :building-number "181"}
+
+      "Refer"
+      {:postal-code nil
+       :unit-number nil
+       :road-name nil
+       :building-number nil}
+
+      "101 Thomson Road, Singapore 307591 (United Square Outlet) | 6254 8123 | Opening Hours for Lunch: 11:30am ~ 2:30pm (Daily Except PH) for Dinner: Sun ~ Thur, PH : 5:30pm ~ 12am Fri, Sat, Day Before PH : 5:30pm ~ 2am"
+      {:postal-code "307591"
+       :unit-number nil
+       :road-name "Thomson Road"
+       :building-number "101"}
+
+      "7 Tan Quee Lan St, Singapore 188096 | Tel number: +65 63362875 | Open daily from 12pm – 12am"
+      {:postal-code "188096"
+       :unit-number nil
+       :road-name "Tan Quee Lan St"
+       :building-number "7"}
+
+      "181 Orchard Road, 08-09/10/11 Orchard Central, Singapore 238896 | Opening hours: Mon – Fri: 11:00 – 15:00 and 18:00 – 22:00 Sat – Sun: 11:30 – 22:30 |"
+      {:postal-code "238896"
+       :unit-number "08-09/10/11"
+       :road-name "Orchard Road"
+       :building-number "181"}
+
+      ""
+      {:postal-code nil
+       :unit-number nil
+       :road-name nil
+       :building-number nil}
+
+      "School Of The Arts Singapore, 1 Zubir Said Drive #01-04/07 Singapore 227968 (Dhoby Ghaut) | Tel: 6238 7218 | Opening hours: Mon – Fri: 11:30 – 14:30, Mon – Fri: 17:00 – 23:00 and Sat – Sun: 11:30 – 23:00"
+      {:postal-code "227968"
+       :unit-number "#01-04/07"
+       :road-name "Zubir Said Drive"
+       :building-number "1"}
+
+      "Plaza Singapura #03-01 68 Orchard Road Singapore 238839 |"
+      {:postal-code "238839"
+       :unit-number "#03-01"
+       :road-name "Orchard Road"
+       :building-number "68"}
+
+      "1 Maritime Square #04-01 HarbourFront Centre"
+      {:postal-code nil
+       :unit-number "#04-01"
+       :road-name "Maritime Square"
+       :building-number "1"})))
