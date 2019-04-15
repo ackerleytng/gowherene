d47f320e75a084a49a784903977cbec032c304ed
ackerleytng <ackerleytng@gmail.com>
2019-04-16 06:15:56 +0800

    Add another label detection method: <strong> tags

--- src/gowherene/reader/label.clj
+++ src/gowherene/reader/label.clj
@@ -1,7 +1,12 @@
 (ns gowherene.reader.label
-  (:require [clojure.zip :as zip]
+  (:require [clojure.string :as str]
+            [clojure.zip :as zip]
             [hickory.select :as hselect]
             [gowherene.reader.utils :refer [content subtree]]))
+
+;; TODO: room for refactorization here, but implementation cannot combine all predicates into one
+;;   because that would check every node for all predicates instead of
+;;   checking all nodes for one predicate first, then the next predicate, and so on
 
 (defn earlier-header
   "Given a loc, find the header just above or before this loc."
@@ -17,13 +22,21 @@
    loc
    (hselect/attr :style #(re-find #"font-size\s*:\s*x-large" %))))
 
+(defn earlier-strong
+  "Given a loc, find the loc before this loc that has is marked <strong>"
+  [loc]
+  (hselect/prev-pred
+   loc
+   (hselect/tag :strong)))
+
 (defn label
   [loc]
   (when-let [found ((some-fn
                      earlier-header
-                     earlier-x-large)
+                     earlier-x-large
+                     earlier-strong)
                     loc)]
-    (content (subtree found))))
+    (str/trim (content (subtree found)))))
 
 (defn add-label
   [{:keys [loc] :as input}]
--- test/gowherene/reader/label_test.clj
+++ test/gowherene/reader/label_test.clj
@@ -30,7 +30,7 @@
                                              [:span {:style "font-size: x-large;"} "126 WEN DAO SHI"]]
                                       [:p "hello"]]]))
                       (iterate zip/next)
-                      (take 8)
+                      (take 9)
                       last  ;; This is the loc of "hello"
                       earlier-x-large
                       zip/node
@@ -44,6 +44,27 @@
   (testing "earlier-x-large when input is nil"
     (is (nil? (earlier-x-large nil)))))
 
+(deftest test-earlier-strong
+  (testing "earlier-strong positive case"
+    (is (= :strong (->> (hickory-zip (hiccup-to-hickory
+                                      [[:body [:p {:style "text-align: justify;"}
+                                               [:strong "Edge"]]
+                                        [:p "hello"]]]))
+                        (iterate zip/next)
+                        (take 9)
+                        last  ;; This is the loc of "hello"
+                        earlier-strong
+                        zip/node
+                        :tag))))
+  (testing "earlier-strong when there isn't an earlier header"
+    (is (nil? (->> (hickory-zip (hiccup-to-hickory [[:p "hello"]]))
+                   (iterate zip/next)
+                   (take 6)
+                   last  ;; This is the loc of "hello"
+                   earlier-strong))))
+  (testing "earlier-strong when input is nil"
+    (is (nil? (earlier-strong nil)))))
+
 (deftest test-label
   (testing "label finds header"
     (is (= "header" (->> (hickory-zip (hiccup-to-hickory [[:body [:h1 "header"] [:p "hello"]]]))
@@ -52,11 +73,19 @@
                          last  ;; This is the loc of "hello"
                          label)))
     (is (= "126 WEN DAO SHI" (->> (hickory-zip (hiccup-to-hickory
+                                                [[:body [:p {:style "text-align: justify;"}
+                                                         [:span {:style "font-size: x-large;"} "126 WEN DAO SHI"]]
+                                                  [:p "hello"]]]))
+                                  (iterate zip/next)
+                                  (take 9)
+                                  last  ;; This is the loc of "hello"
+                                  label)))
+    (is (= "Edge" (->> (hickory-zip (hiccup-to-hickory
                                [[:body [:p {:style "text-align: justify;"}
-                                        [:span {:style "font-size: x-large;"} "126 WEN DAO SHI"]]
+                                        [:strong "Edge \n"]]
                                  [:p "hello"]]]))
                  (iterate zip/next)
-                 (take 8)
+                 (take 9)
                  last  ;; This is the loc of "hello"
                  label))))
   (testing "label when there isn't an earlier header"
