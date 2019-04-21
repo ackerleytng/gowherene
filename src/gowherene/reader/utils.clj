7b87c223c7346d62c3dca904015b1886c5e2ea97
ackerleytng <ackerleytng@gmail.com>
2019-04-21 13:03:18 +0800

    Add tree pruning functions and tests

--- src/gowherene/reader/utils.clj
+++ src/gowherene/reader/utils.clj
@@ -15,6 +15,40 @@
   [loc]
   (hickory-zip (zip/node loc)))
 
+(defn prune-before
+  "Removes all left siblings before loc and returns the same loc in the pruned tree"
+  ([loc] (prune-before loc 1))
+  ([loc levels]
+   (let [parent (zip/up loc)]
+     (if (nil? parent)
+       ;; At the top already, remove the changed tag on the subtree and return
+       (->> (hickory-zip (zip/root loc))
+            (iterate zip/down)
+            (take levels)
+            last)
+       (recur ;; rebuild parent without the lefts of loc
+        (zip/edit
+         parent
+         #(zip/make-node loc % (concat [(zip/node loc)] (zip/rights loc))))
+        (inc levels))))))
+
+(defn prune-after
+  "Removes all right siblings before loc and returns the same loc in the pruned tree"
+  ([loc] (prune-after loc 1))
+  ([loc levels]
+   (let [parent (zip/up loc)]
+     (if (nil? parent)
+       ;; At the top already, remove the changed tag on the subtree and return
+       (->> (hickory-zip (zip/root loc))
+            (iterate (comp zip/rightmost zip/down))
+            (take levels)
+            last)
+       (recur ;; rebuild parent without the rights of loc
+        (zip/edit
+         parent
+         #(zip/make-node loc % (concat (zip/lefts loc) [(zip/node loc)])))
+        (inc levels))))))
+
 (defn content
   [loc]
   (let [c (->> loc
--- test/gowherene/reader/utils_test.clj
+++ test/gowherene/reader/utils_test.clj
@@ -27,3 +27,82 @@
     (is (= "header hello world"
            (->> (hickory-zip (hiccup-to-hickory [[:h1 "header"] [:p [:b "hello"] [:i "world"]]]))
                 content)))))
+
+(deftest test-prune-before
+  (let [subtree (hickory-zip
+                 (hiccup-to-hickory
+                  [[:p
+                    "Thomson Branch"
+                    [:br]
+                    [:strong {:style "line-height: 1.3em;"}
+                     [:strong {:style "color: #d47978;"} "Address:"]
+                     "&nbsp;"]
+                    "187 Upper Thomson Road"
+                    [:br]
+                    [:strong {:style "color: #d47978;"} "Opening Hours:"]
+                    [:strong "&nbsp;"]
+                    "Mon to Thurs: 4pm - 12am |&nbsp;Fri to Sun: 12pm - 12am"
+                    [:br]
+                    [:br]
+                    "Katong Branch"
+                    [:br]
+                    [:span {:style "color: #d47978;"}
+                     [:strong "Address:"]]
+                    " 465 Joo Chiat Road"
+                    [:br]
+                    [:strong {:style "color: #d47978;"} "Opening Hours:"]
+                    [:strong "&nbsp;"]
+                    "Mon to Thurs: 4pm - 12am |&nbsp;Fri to Sun: 12pm - 12am"]]))
+        address-loc (->> subtree
+                         (iterate zip/next)
+                         (take 10)
+                         last)
+        thomson-branch-loc (->> subtree
+                                (iterate zip/next)
+                                (take 6)
+                                last)]
+    (testing "prune-before"
+      (is (= "Address: 187 Upper Thomson Road Opening Hours: Mon to Thurs: 4pm - 12am | Fri to Sun: 12pm - 12am Katong Branch Address: 465 Joo Chiat Road Opening Hours: Mon to Thurs: 4pm - 12am | Fri to Sun: 12pm - 12am"
+             (content (hickory-zip (zip/root (prune-before address-loc)))))))
+    (testing "prune-before at top"
+      (is (= subtree (prune-before subtree))))))
+
+(deftest test-prune-after
+  (let [subtree (hickory-zip
+                 (hiccup-to-hickory
+                  [[:p
+                    "Thomson Branch"
+                    [:br]
+                    [:strong {:style "line-height: 1.3em;"}
+                     [:strong {:style "color: #d47978;"} "Address:"]
+                     "&nbsp;"]
+                    "187 Upper Thomson Road"
+                    [:br]
+                    [:strong {:style "color: #d47978;"} "Opening Hours:"]
+                    [:strong "&nbsp;"]
+                    "Mon to Thurs: 4pm - 12am |&nbsp;Fri to Sun: 12pm - 12am"
+                    [:br]
+                    [:br]
+                    "Katong Branch"
+                    [:br]
+                    [:span {:style "color: #d47978;"}
+                     [:strong "Address:"]]
+                    " 465 Joo Chiat Road"
+                    [:br]
+                    [:strong {:style "color: #d47978;"} "Opening Hours:"]
+                    [:strong "&nbsp;"]
+                    "Mon to Thurs: 4pm - 12am |&nbsp;Fri to Sun: 5am - 5pm"]]))
+        second-br-loc (->> subtree
+                           (iterate zip/next)
+                           (take 13)
+                           last)
+        end (->> subtree
+                 (iterate zip/next)
+                 (take-while (complement zip/end?))
+                 last)]
+    (testing "prune-after"
+      (is (= "Thomson Branch Address: 187 Upper Thomson Road"
+             (content (hickory-zip (zip/root (prune-after second-br-loc)))))))
+    (testing "prune-after-last"
+      (is (= subtree
+             (hickory-zip (zip/root (prune-after end))))))))
